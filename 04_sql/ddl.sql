0efb2f1aedfe5304c6b82fbe8baa70830ada2acc
Joao <jcarapinabarbosa@gmail.com>
2026-01-26 19:34:20 -0300

    Conclusão do DDL

--- 04_sql/ddl.sql
+++ 04_sql/ddl.sql
@@ -32,3 +32,31 @@ CREATE TABLE fornecedores(
     Estado VARCHAR(2) NOT NULL,
     CEP VARCHAR(8) NOT NULL
 );
+
+CREATE TABLE clientes(
+    Id TEXT NOT NULL PRIMARY KEY,
+    Nome VARCHAR(255) NOT NULL,
+    Telefone VARCHAR(20),
+    Email VARCHAR(100) DEFAULT 'Sem email',
+    Endereco VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE pedidos(
+    Id TEXT PRIMARY KEY,
+    IdCliente TEXT NOT NULL,
+    DataHoraPedido DATETIME NOT NULL,
+    Status VARCHAR(50) NOT NULL,
+    FOREIGN KEY (IdCliente) REFERENCES clientes(Id) ON DELETE CASCADE 
+    -- Ao deletar um cliente, todos os seus pedidos também serão deletados
+);
+
+CREATE TABLE itens_pedido(
+    Idpedido TEXT ,
+    Idproduto TEXT ,
+    Quantidade INT NOT NULL,
+    PrecoUnitario DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (Idpedido, Idproduto),
+    FOREIGN KEY (Idpedido) REFERENCES pedidos(Id) ON DELETE CASCADE,
+    FOREIGN KEY (Idproduto) REFERENCES produtos(Id) ON DELETE CASCADE
+    
+);
