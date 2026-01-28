34cdfefb1071856154f7b62db883c230aabec024
Joao <jcarapinabarbosa@gmail.com>
2026-01-27 21:09:14 -0300

    Realizando as primeiras consultas InnerJoin

---
+++ 04_sql/04_innerjoin.sql
@@ -0,0 +1,7 @@
+-- Inner Join é uma operação que combina linhas de duas ou mais tabelas com base em uma condição relacionada entre elas.
+-- Exemplo: Listar todos os pedidos com o nome do cliente
+
+Select t1.Nome, t2.Id, t2.DataHoraPedido
+FROM Clientes AS t1
+INNER JOIN Pedidos AS t2
+ON t1.Id = t2.IdCliente;
